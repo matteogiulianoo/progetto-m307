907b18b5e8a36f7bf20fbff80087c611eb20fef8
Matteo Giuliano <matteo@giuliano.family>
2025-12-17 15:29:10 +0100

    update files

--- public/js/home.js
+++ public/js/home.js
@@ -14,6 +14,17 @@ export async function allThreads() {
     });
 }
 
+/**
+ * Questa funzione restituisce gli ultimi 5 threads inviati da un'utente
+ * @param {*} idCurrentUser 
+ */
 async function personalThreads(idCurrentUser) {
-
+    return await sql("SELECT u.name AS owner, t.title, CASE WHEN CHAR_LENGTH(t.description) > 100 THEN CONCAT(LEFT(t.description, 50), '...' ELSE t.description END AS short_desc FROM threads t JOIN users u ON u.idUser = t.owner WHERE t.owner = ? LIMIT 5", [idCurrentUser])
+        .then(res => {
+            return res;
+        })
+        .catch(e => {
+            console.error(e);
+            throw e;
+        })
 }
